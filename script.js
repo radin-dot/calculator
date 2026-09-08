247ad8b8063d184553e728f17d7bb8070243beba
radin-dot <radin.h.dev@gmail.com>
2026-09-08 19:52:06 +0330

    add html id and some js dom

---
+++ script.js
@@ -0,0 +1,24 @@
+const output = document.getElementById("output");
+
+const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete");
+
+const divideButton = document.getElementById("divide");
+const multiplyButton = document.getElementById("multiply");
+const subtractButton = document.getElementById("subtract");
+const addButton = document.getElementById("add");
+
+const equalButton = document.getElementById("equal");
+
+const zeroButton = document.getElementById("zero");
+const oneButton = document.getElementById("one");
+const twoButton = document.getElementById("two");
+const threeButton = document.getElementById("three");
+const fourButton = document.getElementById("four");
+const fiveButton = document.getElementById("five");
+const sixButton = document.getElementById("six");
+const sevenButton = document.getElementById("seven");
+const eightButton = document.getElementById("eight");
+const nineButton = document.getElementById("nine");
+
+const decimalButton = document.getElementById("decimal");
